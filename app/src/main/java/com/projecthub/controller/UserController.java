3cf3b045b707991af9d22dd6cfeff756b5e8c6d4
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-15 14:41:37 +0100

    feat: add ComponentController for managing components and update ProjectController and UserController for improved data handling

--- app/src/main/java/com/projecthub/controller/UserController.java
+++ app/src/main/java/com/projecthub/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projecthub.dto.UserSummary;
 import com.projecthub.model.User;
 import com.projecthub.service.UserService;
 
@@ -29,7 +30,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody User user) {
+    public String createUser(@Valid @RequestBody UserSummary user) {
         userService.saveUser(user);
         return "User created successfully";
     }
