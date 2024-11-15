3d85171b78c2fe7d62bc0c92a7c4152f6c00bcb5
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-15 14:42:27 +0100

    feat: add findTeamById method to CustomTeamRepository and update save method in CustomUserRepository to accept UserSummary

--- app/src/main/java/com/projecthub/repository/custom/CustomUserRepository.java
+++ app/src/main/java/com/projecthub/repository/custom/CustomUserRepository.java
@@ -3,11 +3,12 @@ package com.projecthub.repository.custom;
 import java.util.List;
 import java.util.Optional;
 
+import com.projecthub.dto.UserSummary;
 import com.projecthub.model.User;
 
 public interface CustomUserRepository {
     List<User> findAll();
-    User save(User user);
+    User save(UserSummary userDTO);
     void deleteById(Long userId);
     Optional<User> findById(Long id);
     List<User> findByTeamId(Long teamId);
