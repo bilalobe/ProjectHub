b53abbff8e3bf266296b0adf9b2f2aa186cdd7ff
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-16 14:37:00 +0100

    fix: update save method in CustomUserRepository to use User model instead of UserSummary

--- app/src/main/java/com/projecthub/repository/custom/CustomUserRepository.java
+++ app/src/main/java/com/projecthub/repository/custom/CustomUserRepository.java
@@ -3,13 +3,13 @@ package com.projecthub.repository.custom;
 import java.util.List;
 import java.util.Optional;
 
-import com.projecthub.dto.UserSummary;
 import com.projecthub.model.User;
 
 public interface CustomUserRepository {
     List<User> findAll();
-    User save(UserSummary userDTO);
+    User save(User user);
     void deleteById(Long userId);
     Optional<User> findById(Long id);
     List<User> findByTeamId(Long teamId);
+    Optional<User> findByUsername(String username);
 }
