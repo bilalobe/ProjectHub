335e3d40b8f01ba2ec84bae82e320ca7c5819b20
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-16 14:37:07 +0100

    feat: add method to find teams by class ID in TeamRepository

--- app/src/main/java/com/projecthub/repository/jpa/TeamRepository.java
+++ app/src/main/java/com/projecthub/repository/jpa/TeamRepository.java
@@ -1,12 +1,13 @@
 package com.projecthub.repository.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projecthub.model.Team;
-import com.projecthub.repository.custom.CustomTeamRepository;
 
-@Repository("postgresTeamRepository")
-public interface TeamRepository extends JpaRepository<Team, Long>, CustomTeamRepository {
-    // Custom query methods can be added here
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findByClassId(Long classId);
 }
