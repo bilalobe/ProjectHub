cea9f8ea197d461c4c93bf3a7c6b8d3aaa3f7a17
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-23 00:37:41 +0100

    chore: add custom exception classes for better error handling

---
+++ app/src/main/java/com/projecthub/exception/InvalidCredentialsException.java
@@ -0,0 +1,19 @@
+package com.projecthub.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception thrown when invalid credentials are provided.
+ */
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+    public InvalidCredentialsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
