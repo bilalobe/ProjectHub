5096f2a842d784321e7229c249d03b719c5430b0
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-23 21:41:11 +0100

    feat: enhance repositories with custom query methods for components and projects

--- app/src/main/java/com/projecthub/repository/jpa/ComponentRepository.java
+++ app/src/main/java/com/projecthub/repository/jpa/ComponentRepository.java
@@ -1,12 +1,13 @@
 package com.projecthub.repository.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projecthub.model.Component;
-import com.projecthub.repository.custom.CustomComponentRepository;
 
-@Repository("postgresComponentRepository")
-public interface ComponentRepository extends JpaRepository<Component, Long>, CustomComponentRepository {
-    // Custom query methods can be added here
+@Repository
+public interface ComponentRepository extends JpaRepository<Component, Long> {
+    List<Component> findByProjectId(Long projectId);
 }
--- app/src/main/java/com/projecthub/repository/jpa/ProjectRepository.java
+++ app/src/main/java/com/projecthub/repository/jpa/ProjectRepository.java
@@ -1,8 +1,8 @@
 package com.projecthub.repository.jpa;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +10,12 @@ import com.projecthub.model.Project;
 import com.projecthub.repository.custom.CustomProjectRepository;
 
 @Repository("postgresProjectRepository")
-public interface ProjectRepository extends JpaRepository<Project, Long>, CustomProjectRepository {
+public interface ProjectRepository extends CustomProjectRepository {
+
     @Override
     List<Project> findAllByTeamId(Long teamId);
 
     @Override
     @Query("SELECT p FROM Project p JOIN FETCH p.components WHERE p.id = :projectId")
-    Project findProjectWithComponentsById(Long projectId);
+    Optional<Project> findProjectWithComponentsById(Long projectId);
 }
