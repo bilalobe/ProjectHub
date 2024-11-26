ab2649e1ce5008ca53db24724c5335db5c1d1e97
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-26 23:49:25 +0100

    refactor: simplify loadComponents method in ProjectDetailsController for improved readability and maintainability

--- app/src/main/java/com/projecthub/ui/controllers/details/ProjectDetailsController.java
+++ app/src/main/java/com/projecthub/ui/controllers/details/ProjectDetailsController.java
@@ -1,24 +1,18 @@
 package com.projecthub.ui.controllers.details;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.projecthub.dto.ComponentSummary;
 import com.projecthub.dto.ProjectSummary;
 import com.projecthub.ui.viewmodels.ProjectHubViewModel;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 
 @Component
 public class ProjectDetailsController {
@@ -78,9 +72,8 @@ public class ProjectDetailsController {
      * @param projectId the ID of the project
      */
     private void loadComponents(Long projectId) {
-        List<ComponentSummary> components = viewModel.getComponentService().getComponentsByProjectId(projectId);
-        componentList.setAll(components);
-        componentsTable.setItems(componentList);
+        viewModel.loadComponents(projectId);
+        componentsTable.setItems(viewModel.getComponents());
     }
 
     /**
