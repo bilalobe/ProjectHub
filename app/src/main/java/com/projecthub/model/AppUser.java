5f3a9dca7e7d1eb448d1bb7856a7b08795daa826
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-26 23:08:43 +0100

    refactor: add first name, last name, and email fields to AppUser model with validation

--- app/src/main/java/com/projecthub/model/AppUser.java
+++ app/src/main/java/com/projecthub/model/AppUser.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -41,6 +42,28 @@ public class AppUser {
     private String password;
 
     /**
+     * The first name of the user.
+     */
+    @NotBlank(message = "First name is mandatory")
+    @Size(max = 50, message = "First name must be less than 50 characters")
+    private String firstName;
+
+    /**
+     * The last name of the user.
+     */
+    @NotBlank(message = "Last name is mandatory")
+    @Size(max = 50, message = "Last name must be less than 50 characters")
+    private String lastName;
+
+    /**
+     * The email of the user.
+     */
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
+    private String email;
+
+    /**
      * The team that the user belongs to.
      */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -52,17 +75,23 @@ public class AppUser {
     }
 
     // Constructor with all fields
-    public AppUser(Long id, String username, String password, Team team) {
+    public AppUser(Long id, String username, String password, String firstName, String lastName, String email, Team team) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
         this.team = team;
     }
 
     // Constructor without id (for new Users)
-    public AppUser(String username, String password, Team team) {
+    public AppUser(String username, String password, String firstName, String lastName, String email, Team team) {
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
         this.team = team;
     }
 
@@ -89,6 +118,30 @@ public class AppUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Team getTeam() {
