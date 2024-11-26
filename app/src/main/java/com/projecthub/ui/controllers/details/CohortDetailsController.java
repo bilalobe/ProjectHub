48487907fcc8155cf30d8a40076a66cbdb17d75b
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-26 23:40:52 +0100

    refactor: update CohortDetailsController to improve cohort management with save and delete functionality

--- app/src/main/java/com/projecthub/ui/controllers/details/CohortDetailsController.java
+++ app/src/main/java/com/projecthub/ui/controllers/details/CohortDetailsController.java
@@ -1,18 +1,13 @@
 package com.projecthub.ui.controllers.details;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.projecthub.dto.CohortSummary;
 import com.projecthub.dto.TeamSummary;
 import com.projecthub.ui.viewmodels.CohortViewModel;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CohortDetailsController {
@@ -21,13 +16,16 @@ public class CohortDetailsController {
     private CohortViewModel cohortViewModel;
 
     @FXML
+    private Label cohortIdLabel;
+
+    @FXML
     private Label cohortNameLabel;
 
     @FXML
     private Label schoolNameLabel;
 
     @FXML
-    private TableView<TeamSummary> teamTableView;
+    private TableView<TeamSummary> teamsTableView;
 
     @FXML
     private TableColumn<TeamSummary, Long> teamIdColumn;
@@ -36,24 +34,40 @@ public class CohortDetailsController {
     private TableColumn<TeamSummary, String> teamNameColumn;
 
     @FXML
-    private Button addTeamButton;
+    private Button saveCohortButton;
+
+    @FXML
+    private Button deleteCohortButton;
 
     @FXML
     public void initialize() {
+        cohortIdLabel.textProperty().bind(cohortViewModel.cohortIdProperty().asString());
         cohortNameLabel.textProperty().bind(cohortViewModel.cohortNameProperty());
-        schoolNameLabel.textProperty().bind(cohortViewModel.schoolNameProperty());
+        schoolNameLabel.textProperty().bind(cohortViewModel.schoolProperty().asString());
 
         teamIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         teamNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        teamTableView.setItems(cohortViewModel.getTeams());
 
-        addTeamButton.setOnAction(event -> handleAddTeam());
+        teamsTableView.setItems(cohortViewModel.getTeams());
+
+        saveCohortButton.setOnAction(event -> saveCohort());
+        deleteCohortButton.setOnAction(event -> deleteCohort());
     }
 
     public void setCohort(CohortSummary cohort) {
         cohortViewModel.setCohort(cohort);
     }
 
-    private void handleAddTeam() {
+    private void saveCohort() {
+        CohortSummary cohortSummary = new CohortSummary(
+                cohortViewModel.cohortIdProperty().get(),
+                cohortViewModel.cohortNameProperty().get(),
+                cohortViewModel.schoolProperty().get()
+        );
+        cohortViewModel.saveCohort(cohortSummary);
+    }
+
+    private void deleteCohort() {
+        cohortViewModel.deleteCohort(cohortViewModel.cohortIdProperty().get());
     }
 }
