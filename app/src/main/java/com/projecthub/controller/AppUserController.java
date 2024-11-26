6e4ce1a08b0885d5536edef723aa7382bee48035
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-26 12:57:08 +0100

    refactor: enhance AppUserController with Swagger annotations for improved API documentation

--- app/src/main/java/com/projecthub/controller/AppUserController.java
+++ app/src/main/java/com/projecthub/controller/AppUserController.java
@@ -1,6 +1,10 @@
 package com.projecthub.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,17 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.projecthub.dto.AppUserSummary;
 import com.projecthub.exception.ResourceNotFoundException;
 import com.projecthub.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 
 @Tag(name = "User API", description = "Operations pertaining to users in ProjectHub")
 @RestController
@@ -28,11 +29,13 @@ public class AppUserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Get all users")
     @GetMapping
     public List<AppUserSummary> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Get user by ID")
     @GetMapping("/{id}")
     public ResponseEntity<AppUserSummary> getUserById(@PathVariable Long id) {
         Optional<AppUserSummary> user = userService.getUserById(id);
