a61f7ab4d9289a20f496348c3e1e64f86cef00e4
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-30 23:00:27 +0100

    feat: add custom RepositoryException for repository operations

---
+++ app/src/main/java/com/projecthub/exception/RepositoryException.java
@@ -0,0 +1,14 @@
+package com.projecthub.exception;
+
+/**
+ * Custom exception for repository operations.
+ */
+public class RepositoryException extends RuntimeException {
+    public RepositoryException(String message) {
+        super(message);
+    }
+
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
