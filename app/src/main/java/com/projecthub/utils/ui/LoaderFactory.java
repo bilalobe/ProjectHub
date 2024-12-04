2bf1c108904c088ea63bbe04cdbe0d3b0cea8a46
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-04 12:05:08 +0100

    feat: refactor LoaderFactory to use DTOs instead of Summary classes

--- app/src/main/java/com/projecthub/utils/ui/LoaderFactory.java
+++ app/src/main/java/com/projecthub/utils/ui/LoaderFactory.java
@@ -3,9 +3,9 @@ package com.projecthub.utils.ui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.projecthub.dto.CohortSummary;
-import com.projecthub.dto.SchoolSummary;
-import com.projecthub.dto.TeamSummary;
+import com.projecthub.dto.CohortDTO;
+import com.projecthub.dto.SchoolDTO;
+import com.projecthub.dto.TeamDTO;
 import com.projecthub.utils.ui.loaders.CohortLoader;
 import com.projecthub.utils.ui.loaders.SchoolLoader;
 import com.projecthub.utils.ui.loaders.TeamLoader;
@@ -23,11 +23,11 @@ public class LoaderFactory {
     private TeamLoader teamLoader;
 
     public TreeItemLoader getLoader(Object data) {
-        if (data instanceof SchoolSummary) {
+        if (data instanceof SchoolDTO) {
             return schoolLoader;
-        } else if (data instanceof CohortSummary) {
+        } else if (data instanceof CohortDTO) {
             return cohortLoader;
-        } else if (data instanceof TeamSummary) {
+        } else if (data instanceof TeamDTO) {
             return teamLoader;
         }
         return null;
