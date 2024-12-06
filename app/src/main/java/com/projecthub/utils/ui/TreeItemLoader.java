78eb540eccb4068eb687089438090555285de77a
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-06 21:10:18 +0100

    feat: refactor TreeItemLoader; rename loadChildren method to load and update parameter usage for improved clarity

--- app/src/main/java/com/projecthub/utils/ui/TreeItemLoader.java
+++ app/src/main/java/com/projecthub/utils/ui/TreeItemLoader.java
@@ -3,5 +3,5 @@ package com.projecthub.utils.ui;
 import javafx.scene.control.TreeItem;
 
 public interface TreeItemLoader {
-    void loadChildren(TreeItem<TreeItemWrapper> parentItem);
+    void load(TreeItem<TreeItemWrapper> safeTreeItem, TreeItemWrapper parentWrapper);
 }
