bb82eedf11ec2d6b9240687c4dc9b90937b9268c
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-06 13:47:58 +0100

    feat(user-management): add UserManagementComponent with HTML template, service integration, and unit tests

---
+++ frontend/src/app/components/user-management/user-management.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.css']
+})
+export class UserManagementComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+}
