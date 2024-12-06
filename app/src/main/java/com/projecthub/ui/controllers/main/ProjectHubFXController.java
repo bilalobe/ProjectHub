4f74917e30a4ad54443b66aa2fb2c1827631d606
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-06 21:09:57 +0100

    feat: remove unnecessary @Autowired annotation from ProjectHubFXController

--- app/src/main/java/com/projecthub/ui/controllers/main/ProjectHubFXController.java
+++ app/src/main/java/com/projecthub/ui/controllers/main/ProjectHubFXController.java
@@ -1,6 +1,5 @@
 package com.projecthub.ui.controllers.main;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.projecthub.utils.ui.LoaderFactory;
@@ -20,7 +19,7 @@ public class ProjectHubFXController {
     
     private ProjectHubViewModel viewModel;
 
-    @Autowired
+
     private LoaderFactory loaderFactory;
 
     @FXML
