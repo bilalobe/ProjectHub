0de05bce08a6c22f636052af15f70f509c047537
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-09 13:54:38 +0100

    feat: add JPA repository support and main method to ProjectHubApplication

--- app/src/main/java/com/projecthub/ProjectHubApplication.java
+++ app/src/main/java/com/projecthub/ProjectHubApplication.java
@@ -1,10 +1,17 @@
 package com.projecthub;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.projecthub.config.CsvProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CsvProperties.class)
+@EnableJpaRepositories(basePackages = "com.projecthub.repository.jpa")
 public class ProjectHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHubApplication.class, args);
+    }
 }
