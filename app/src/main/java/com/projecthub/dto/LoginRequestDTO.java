0da4bd0f4091bc41e967da8e17f663e5849d06a2
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:17:26 +0100

    feat: add LoginRequestDTO for user login request data representation with validation

---
+++ app/src/main/java/com/projecthub/dto/LoginRequestDTO.java
@@ -0,0 +1,21 @@
+package com.projecthub.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for user login requests.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+}
