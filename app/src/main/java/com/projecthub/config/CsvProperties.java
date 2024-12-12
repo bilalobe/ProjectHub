8c434b3925f3430c58c38b32c9852542b4ed2d42
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:15:08 +0100

    refactor: simplify CsvProperties by using Lombok annotations for getters and setters

--- app/src/main/java/com/projecthub/config/CsvProperties.java
+++ app/src/main/java/com/projecthub/config/CsvProperties.java
@@ -1,8 +1,12 @@
 package com.projecthub.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "app")
 
@@ -19,83 +23,4 @@ public class CsvProperties {
     private String cohortsFilepath;
     private String usersFilepath;
 
-    public String getComponentsFilepath() {
-        return componentsFilepath;
-    }
-
-    public void setComponentsFilepath(String componentsFilepath) {
-        this.componentsFilepath = componentsFilepath;
-    }
-
-    public String getProjectsFilepath() {
-        return projectsFilepath;
-    }
-
-    public void setProjectsFilepath(String projectsFilepath) {
-        this.projectsFilepath = projectsFilepath;
-    }
-
-    public String getSchoolsFilepath() {
-        return schoolsFilepath;
-    }
-
-    public void setSchoolsFilepath(String schoolsFilepath) {
-        this.schoolsFilepath = schoolsFilepath;
-    }
-
-    public String getStudentsFilepath() {
-        return studentsFilepath;
-    }
-
-    public void setStudentsFilepath(String studentsFilepath) {
-        this.studentsFilepath = studentsFilepath;
-    }
-
-    public String getSubmissionsFilepath() {
-        return submissionsFilepath;
-    }
-
-    public void setSubmissionsFilepath(String submissionsFilepath) {
-        this.submissionsFilepath = submissionsFilepath;
-    }
-
-    public String getTasksFilepath() {
-        return tasksFilepath;
-    }
-
-    public void setTasksFilepath(String tasksFilepath) {
-        this.tasksFilepath = tasksFilepath;
-    }
-
-    public String getTeachersFilepath() {
-        return teachersFilepath;
-    }
-
-    public void setTeachersFilepath(String teachersFilepath) {
-        this.teachersFilepath = teachersFilepath;
-    }
-
-    public String getTeamsFilepath() {
-        return teamsFilepath;
-    }
-
-    public void setTeamsFilepath(String teamsFilepath) {
-        this.teamsFilepath = teamsFilepath;
-    }
-
-    public String getCohortsFilepath() {
-        return cohortsFilepath;
-    }
-
-    public void setCohortsFilepath(String cohortsFilepath) {
-        this.cohortsFilepath = cohortsFilepath;
-    }
-
-    public String getUsersFilepath() {
-        return usersFilepath;
-    }
-
-    public void setUsersFilepath(String usersFilepath) {
-        this.usersFilepath = usersFilepath;
-    }
 }
