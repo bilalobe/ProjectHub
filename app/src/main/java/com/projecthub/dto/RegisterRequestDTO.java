2da7421cc325f896f4c4aee9630300d22745de84
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:17:55 +0100

    feat: add RegisterRequestDTO for user registration request data representation with validation

---
+++ app/src/main/java/com/projecthub/dto/RegisterRequestDTO.java
@@ -0,0 +1,30 @@
+package com.projecthub.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequestDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 100)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 100)
+    private String lastName;
+}
