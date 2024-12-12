68df9206f8baf124c089a68137be3b6964e38ea9
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:15:47 +0100

    feat: enhance SecurityConfig with AuthenticationManager and constructor injection for Environment

--- app/src/main/java/com/projecthub/config/SecurityConfig.java
+++ app/src/main/java/com/projecthub/config/SecurityConfig.java
@@ -3,7 +3,8 @@ package com.projecthub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,12 +17,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-    private Environment env;
+    private final Environment env;
+
+    public SecurityConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -46,16 +53,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/public/**").permitAll() // Allow public access to specific URLs
-                    .anyRequest().authenticated()
+                        .requestMatchers("/public/**").permitAll() // Allow public access to specific URLs
+                        .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults())
                 .oauth2Login(withDefaults())
                 .httpBasic(withDefaults())
                 .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Use stateless sessions
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Use stateless sessions
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
