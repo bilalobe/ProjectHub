59292e873e5e55ef1244131bcfda674757ea418e
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:25:00 +0100

    feat: add BaseEntity class for soft delete functionality with auditing support

---
+++ app/src/main/java/com/projecthub/model/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.projecthub.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.jpa.domain.AbstractAuditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@FilterDef(name = "deletedFilter", parameters = @ParamDef(name = "isDeleted", type = Boolean.class))
+@Filter(name = "deletedFilter", condition = "deleted = :isDeleted")
+@SQLDelete(sql = "UPDATE {h-schema}{h-table} SET deleted = true WHERE id = ?")
+@Getter
+@Setter
+@ToString
+public abstract class BaseEntity extends AbstractAuditable<AppUser, UUID> {
+
+    @Column(name = "deleted", nullable = false)
+    private final boolean deleted = false;
+}
