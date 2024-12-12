c545788028a4066d06a8692495470a11652e3eed
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-13 00:17:45 +0100

    feat: add PasswordChangeRequestDTO for password change request data representation with validation

---
+++ app/src/main/java/com/projecthub/dto/PasswordChangeRequestDTO.java
@@ -0,0 +1,21 @@
+package com.projecthub.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for password change requests.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordChangeRequestDTO {
+
+    @NotBlank(message = "Old password is mandatory")
+    private String oldPassword;
+
+    @NotBlank(message = "New password is mandatory")
+    private String newPassword;
+}
