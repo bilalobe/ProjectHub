b76837e3c1cab79a9262b19331ad2ea0385b6521
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-19 18:14:12 +0100

    refactor: Major overhaul for core structure separation

--- core/src/main/java/com/projecthub/core/services/ui/snackbar/SnackbarService.java
+++ core/src/main/java/com/projecthub/core/services/ui/snackbar/SnackbarService.java
@@ -1,7 +1,6 @@
 package com.projecthub.core.services.ui.snackbar;
 
 import com.gluonhq.charm.glisten.control.Snackbar;
-import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.application.Platform;
