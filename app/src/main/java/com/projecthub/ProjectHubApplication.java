c3fef5b157f8882f0c710c7d34d3f30f74ba4356
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-19 11:42:13 +0100

    refactor: remove SpringBootApplication annotation from ProjectHubApplication

--- app/src/main/java/com/projecthub/ProjectHubApplication.java
+++ app/src/main/java/com/projecthub/ProjectHubApplication.java
@@ -1,13 +1,11 @@
 package com.projecthub;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * The main class for the Project Hub application.
  */
-@SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.projecthub.repository")
 public class ProjectHubApplication {
 
