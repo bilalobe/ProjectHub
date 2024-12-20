d5aae27f363d61b03e3a1e8c40c319c4ad7fba3d
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-12-20 13:15:37 +0100

    feat: add main application class for Project Hub with JPA repository support

---
+++ core/src/main/java/com/projecthub/ProjectHubApplication.java
@@ -0,0 +1,15 @@
+package com.projecthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * The main class for the Project Hub application.
+ */
+@EnableJpaRepositories(basePackages = "com.projecthub.repository")
+public class ProjectHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHubApplication.class, args);
+    }
+}
