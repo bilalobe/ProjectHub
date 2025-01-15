f6c8d00b208ed57baea3faee02dabe6a2f1d44a4
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-01-15 11:29:02 +0100

    refactor: Improve dependency declarations and repository configuration in Gradle scripts

--- buildSrc/src/main/groovy/com/projecthub/gradle/convention/BaseConventionPlugin.groovy
+++ buildSrc/src/main/groovy/com/projecthub/gradle/convention/BaseConventionPlugin.groovy
@@ -3,7 +3,7 @@ package com.projecthub.gradle.convention
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.GradleException
-import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.JavaExec
@@ -14,13 +14,13 @@ class BaseConventionPlugin implements Plugin<Project> {
         // Apply core plugins
         project.plugins.apply('java-library')
         project.plugins.apply('distribution')
-        
+
         // Load dependencies
         applyDependencies(project)
 
         // Configure tasks
         project.tasks.withType(JavaCompile).configureEach {
-            
+
             options.encoding = 'UTF-8'
             options.compilerArgs += '--enable-preview'
         }
@@ -44,12 +44,12 @@ class BaseConventionPlugin implements Plugin<Project> {
                     'Build-Timestamp': new Date().format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
                 )
             }
-            jarTask.setDuplicatesStrategy(org.gradle.api.file.DuplicatesStrategy.EXCLUDE)
+            jarTask.setDuplicatesStrategy(DuplicatesStrategy.EXCLUDE)
         }
 
         configureSharedDependencies(project)
     }
-    
+
     private void applyDependencies(Project project) {
         def dependenciesFile = new File("${project.rootProject.projectDir}/buildSrc/src/main/groovy/dependencies.gradle")
         if (dependenciesFile.exists()) {
@@ -63,23 +63,15 @@ class BaseConventionPlugin implements Plugin<Project> {
     private void configureSharedDependencies(Project project) {
         project.dependencies {
             // Spring Boot Platform
-            implementation platform("org.springframework.boot:spring-boot-dependencies:${project.ext.versions.springBoot}")
+            implementation(platform("org.springframework.boot:spring-boot-dependencies:${project.ext.versions.springBoot}"))
             
-            // Modulith Core
-            implementation "org.springframework.modulith:spring-modulith-starter-core:${project.ext.versions.springModulith}"
-            implementation "org.springframework.modulith:spring-modulith-api:${project.ext.versions.springModulith}"
-            implementation "org.springframework.modulith:spring-modulith-events-core:${project.ext.versions.springModulith}"
-            
-            // Common Tools
-            implementation "org.mapstruct:mapstruct:${project.ext.versions.mapstruct}"
-            compileOnly "org.projectlombok:lombok:${project.ext.versions.lombok}"
-            annotationProcessor "org.projectlombok:lombok:${project.ext.versions.lombok}"
-            annotationProcessor "org.mapstruct:mapstruct-processor:${project.ext.versions.mapstruct}"
+            // Modulith
+            implementation("org.springframework.modulith:spring-modulith-starter-core:${project.ext.versions.springModulith}")
+            implementation("org.springframework.modulith:spring-modulith-api:${project.ext.versions.springModulith}")
+            implementation("org.springframework.modulith:spring-modulith-events-core:${project.ext.versions.springModulith}")
             
             // Testing
-            testImplementation "org.springframework.boot:spring-boot-starter-test:${project.ext.versions.springBoot}"
-            testImplementation "org.springframework.modulith:spring-modulith-test:${project.ext.versions.springModulith}"
-            testImplementation "org.springframework.modulith:spring-modulith-docs:${project.ext.versions.springModulith}"
+            testImplementation("org.springframework.modulith:spring-modulith-test:${project.ext.versions.springModulith}")
         }
     }
 }
